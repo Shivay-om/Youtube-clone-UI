eb99d8b145dc666fb4d5d0e17f22489efbf25635
Sachin Tyagi <14.sachintyagi@gmail.com>
2024-01-05 10:13:09 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'material-icons/iconfont/material-icons.css';
+import Home from './cmp/home';
+import NotFound from './cmp/not-found';
+
+const App = ()=>{
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
